25f6e437cf1a15d2ca71f4c34c95973b0d0f5272
romkravets <romann.kravets@gmail.com>
2021-02-26 21:48:37 +0200

    Basic setup and homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+
+import './default.scss'
 
 function App() {
   return (
     <div className="App">
-      hello
+        <Header/>
+        <Homepage/>
     </div>
   );
 }
