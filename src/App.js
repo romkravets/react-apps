c32e6139ceb019d3a6c71141e4717f6a6f6807a6
romkravets <romann.kravets@gmail.com>
2021-02-27 20:58:55 +0200

    Google Sign-In

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { auth, handleUserProfile } from './firebase/utils';
 
 //pages
 import Homepage from './pages/Homepage';
@@ -12,28 +13,70 @@ import HomepageLayout from './layouts/HomepageLayout'
 
 import './default.scss';
 
-function App() {
-  return (
-    <div className="App">
-        <Switch>
-            <Route exact path="/"  render={() => (
-              <HomepageLayout>
-                <Homepage/>
-              </HomepageLayout>
-              )}/>
-            <Route path="/registration" render={() => (
-              <MainLayout>
-                <Registration/>
-              </MainLayout>
-              )}/>
-            <Route path="/login" render={() => (
-              <MainLayout>
-                <Login/>
-              </MainLayout>
-            )}/>
-          </Switch>
-    </div>
+const initialState = {
+  currentUser: null
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState
+    };
+  }
+
+  authLisener = null;
+
+  componentDidMount() {
+    this.authLisener = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+      this.setState({
+        ...initialState
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authLisener();
+  }
+
+  render() {
+      const { currentUser } = this.state;
+      
+      return (
+        <div className="App">
+            <Switch>
+                <Route exact path="/"  render={() => (
+                  <HomepageLayout currentUser={currentUser}>
+                    <Homepage/>
+                  </HomepageLayout>
+                  )}/>
+                <Route path="/registration" render={() => (
+                  <MainLayout currentUser={currentUser}>
+                    <Registration/>
+                  </MainLayout>
+                  )}/>
+                <Route path="/login"
+                  render={() => currentUser ? <Redirect to="/" /> : (
+                    <MainLayout currentUser={currentUser}>
+                      <Login/>
+                    </MainLayout>
+                )}/>
+              </Switch>
+        </div>
   );
+  }
+
 }
 
 export default App;
